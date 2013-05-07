8f915795512e4620b7d6ef3f385e3261b4d54bc0
Hendra Uzia <hendra.uzia@yahoo.co.id>
2013-05-07 17:14:55 +0700

    Add example for custom html attributes.
    Signed-off-by: Hendra Uzia <hendra.uzia@yahoo.co.id>

---
+++ example/single_with_custom_attributes.php
@@ -0,0 +1,30 @@
+<?php
+require_once "../DynamicDD.php";
+
+if (!empty($_POST)){
+  echo "<pre>";
+  print_r($_POST);
+  echo "</pre>";
+}
+$dd1 = new DynamicDD(['select_attribute' => 'class="shinny gold" style="z-index:1;"']);
+?>
+<!doctype html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>DROPDOWN SINGLE</title>
+<link rel="stylesheet" href="../css/styles.css">
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+</head>
+<body>
+<div>
+<p>Dropdown Demo - Single</p>
+<form method="post">
+<fieldset>
+<?php echo ($dd1->generateDD());?>
+<input type="submit" value="Send" name="submit" />
+</fieldset>
+<?php echo $dd1->generateJS(); ?>
+</form>
+</body>
+</html>
